c762669c0d852fcc32448816b1fc991a58affa7e
Francis James Lagang <francisjameslagang@syn-172-100-040-058.res.spectrum.com>
2024-07-26 16:11:21 +0800

    jwt-updated

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,18 +2,31 @@ const jwt = require("jsonwebtoken");
 const { secretKey } = require("../config/auth");
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
     return res
       .status(403)
       .json({ success: false, message: "Token is not provided" });
   }
-  jwt.verify(token.split(" ")[1], secretKey, (err, decoded) => {
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(403).json({ success: false, message: "Malformed token" });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
     if (err) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Unauthorized access" });
+      let message = "Unauthorized access";
+      if (err.name === "TokenExpiredError") {
+        message = "Token has expired";
+      } else if (err.name === "JsonWebTokenError") {
+        message = "Invalid token";
+      }
+      return res.status(401).json({ success: false, message });
     }
+
     req.user_mail = decoded.user_mail;
     next();
   });
