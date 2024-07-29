ba65f4ab50f0902380cbfac1f61edfcf69e4cae9
Francis James Lagang <francisjameslagang@syn-172-100-040-058.res.spectrum.com>
2024-07-29 14:13:50 +0800

    added secret key again

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
-const secretKey = crypto.randomBytes(32).toString("hex");
+const secretKey =
+  process.env.JWT_SECRET_KEY || crypto.randomBytes(32).toString("hex");
 const tokenExpiration = "30d";
 
 module.exports = { secretKey, tokenExpiration };
