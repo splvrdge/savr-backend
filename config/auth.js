8f081166cd2483c74e6a9d2b5c0d35687e6fb9f0
Francis James Lagang <francisjameslagang@syn-172-100-040-058.res.spectrum.com>
2024-07-29 14:18:08 +0800

    renamed JWT_SECRET_KEY

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 const secretKey =
-  process.env.JWT_SECRET_KEY || crypto.randomBytes(32).toString("hex");
+  process.env.SECRET_KEY || crypto.randomBytes(32).toString("hex");
 const tokenExpiration = "30d";
 
 module.exports = { secretKey, tokenExpiration };
