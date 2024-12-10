6003315305cbdfb5a5890aaa319e2da92790a490
Francis James Lagang <splurdge.art@gmail.com>
2024-12-10 08:41:44 +0800

    update: refresh tokens

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
-const volumeRoutes = require("./routes/volumeRoutes");
-const bookmarkRoutes = require("./routes/bookmarkRoutes");
-const searchRoutes = require("./routes/searchRoutes");
+const cron = require("node-cron");
+const db = require("./config/db"); // Adjust the path if necessary
 
 const app = express();
-const PORT = 3000;
+const SERVER_PORT = process.env.SERVER_PORT;
+
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/volume", volumeRoutes);
-app.use("/api/bookmark", bookmarkRoutes);
-app.use("/api/search", searchRoutes);
+
+cron.schedule("0 0 * * *", async () => {
+  try {
+    const query = `DELETE FROM tokens WHERE expires_at <= NOW()`;
+    await db.execute(query);
+    console.log("Expired tokens cleaned up");
+  } catch (err) {
+    console.error("Error cleaning up tokens:", err);
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${SERVER_PORT}`);
 });
--- routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const searchController = require("../controllers/searchController");
-
-const router = express.Router();
-
-router.get("/sample-terms", searchController.searchSampleTerms);
-router.get("/skeletal-system", searchController.searchSkeletalSystemTerms);
-
-module.exports = router;
