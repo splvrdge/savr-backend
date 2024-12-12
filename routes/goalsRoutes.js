7254d9732f9262cf2c6afae134d7142e293b6d46
Francis James Lagang <splurdge.art@gmail.com>
2024-12-12 20:37:33 +0800

    add: goals routes

--- routes/goalsRoutes.js
+++ routes/goalsRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const goalController = require("../controllers/goalsController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
+// Goal routes
 router.post("/add", authMiddleware.verifyToken, goalController.addGoal);
 router.get("/:user_id", authMiddleware.verifyToken, goalController.getGoals);
 router.put("/update", authMiddleware.verifyToken, goalController.updateGoal);
-router.delete(
-  "/delete/:goal_id",
-  authMiddleware.verifyToken,
-  goalController.deleteGoal
-);
+router.delete("/delete/:goal_id", authMiddleware.verifyToken, goalController.deleteGoal);
+
+// Contribution routes
+router.post("/contribution/add", authMiddleware.verifyToken, goalController.addContribution);
+router.get("/contributions/:goal_id", authMiddleware.verifyToken, goalController.getContributions);
+router.delete("/contribution/:contribution_id", authMiddleware.verifyToken, goalController.deleteContribution);
 
 module.exports = router;
