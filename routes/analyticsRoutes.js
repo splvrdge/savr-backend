52a5be953d059f203b7c076a1f3ecf4856b2e7c0
Francis James Lagang <splurdge.art@gmail.com>
2024-12-13 12:33:09 +0800

    fix: analytics authMiddleware

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const analyticsController = require('../controllers/analyticsController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
 // Analytics routes
 router.get('/expenses/:user_id', authenticateToken, analyticsController.getExpensesByCategory);
