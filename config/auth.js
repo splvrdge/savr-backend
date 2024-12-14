8464bac7a1ff972928d9e661dde9443ce6372d7c
Francis James Lagang <splurdge.art@gmail.com>
2024-12-15 01:19:17 +0800

    fix: random secret key

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 const crypto = require("crypto");
 
 const secretKey =
-  process.env.SECRET_KEY || crypto.randomBytes(32).toString("hex");
+  process.env.SECRET_KEY || "savr_development_secret_key";
 const refreshTokenSecret =
-  process.env.REFRESH_TOKEN_SECRET || crypto.randomBytes(32).toString("hex");
+  process.env.REFRESH_TOKEN_SECRET || "savr_development_refresh_secret_key";
 
 const tokenExpiration = "15m";
 const refreshTokenExpiration = "7d";
