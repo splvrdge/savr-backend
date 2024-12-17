9512baab900e3c1c1075a18e0e7f3fa333c4c382
Francis James Lagang <splurdge.art@gmail.com>
2024-12-17 21:37:58 +0800

    clean code

--- migrations/insert_test_budgets.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO budgets (user_id, category, budget_limit)
-VALUES 
-  ({{user_id}}, 'Food', 500.00),
-  ({{user_id}}, 'Transportation', 300.00),
-  ({{user_id}}, 'Entertainment', 200.00);
